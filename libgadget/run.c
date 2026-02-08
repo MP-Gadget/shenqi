f5ad409cc6f5c83db2c8c936ac03351e3e5087ca
Simeon Bird <sbird@ucr.edu>
2026-02-08 15:42:53 -0800

    Use the new grav treewalk

--- libgadget/run.c
+++ libgadget/run.c
@@ -544,7 +544,7 @@ run(const int RestartSnapNum, const inttime_t ti_init, const struct header_data 
                     /* Do a short range pairwise only step if desired*/
                     const double rho0 = All.CP.Omega0 * 3 * All.CP.Hubble * All.CP.Hubble / (8 * M_PI * All.CP.GravInternal);
                     force_tree_full(&Tree, ddecomp, HybridNuTracer, All.OutputDir);
-                    grav_short_tree_old(&Act, &pm, &Tree, NULL, rho0, times.Ti_Current);
+                    grav_short_tree(&Act, &pm, &Tree, NULL, rho0, times.Ti_Current);
                     force_tree_free(&Tree);
             }
         }
