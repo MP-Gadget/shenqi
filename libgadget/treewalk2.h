75be8b4e80a0c0a52a3b8ab1c5a9b0b2b884f510
Simeon Bird <sbird@ucr.edu>
2026-02-20 18:44:41 -0800

    Correctly map the tree and particle tables to GPU in ev_primary_gpu

--- libgadget/treewalk2.h
+++ libgadget/treewalk2.h
@@ -555,7 +555,7 @@ private:
          * The output results should be reduced into arrays on output (these stay on the GPU) and some of the arrays copied back to the parts table on the CPU in postprocess.
          * For now map parts to GPU before each treewalk, this would normally be slow, but is ok because we are unified memory.
          */
-        #pragma omp target teams distribute reduction(+: Ninteractions) nowait depend(in: tree) depend(inout: parts) depend(inout: priv)
+        #pragma omp target teams distribute reduction(+: Ninteractions) nowait map(to: tree) map(tofrom: parts) map(tofrom: SlotsManager->info[0].ptr) map(tofrom: SlotsManager->info[5].ptr) depend(in: priv) depend(out: output)
         for(int k = 0; k < WorkSetSize; k++) {
             const int i = WorkSet ? WorkSet[k] : k;
             /* Primary never uses node list */
