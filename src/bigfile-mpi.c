ff79d2fb4f6c4fc6955286bb601fc9c1e7e3980a
Simeon Bird <sbird@ucr.edu>
2026-04-08 13:03:14 -0700

    Squashed 'depends/bigfile/' changes from 37c4a8b3..44c12ec2
    44c12ec2 Avoid a potential overflow in big_block_read/write
    5546cf1e Compute checksum after the write
    c37369aa A similar reduction in fopen calls to the last commit for big_block_read
    5168500a Open a file in big_block_write outside the chunk write loop
    74c4625a Use a flag to specify writing/reading in big_block_mpi_write/read instead of a function pointer
    4026a96c Fix a potential error-path memory leak from faaeff28
    798dee84 Remove unused debug variable
    
    git-subtree-dir: depends/bigfile
    git-subtree-split: 44c12ec2ce7882dbbb226faa974002426d148d2f

--- src/bigfile-mpi.c
+++ src/bigfile-mpi.c
@@ -9,7 +9,6 @@
 
 /* disable aggregation by default */
 static size_t _BigFileAggThreshold = 0;
-static int _big_file_mpi_verbose = 0;
 
 static int big_block_mpi_broadcast(BigBlock * bb, int root, MPI_Comm comm);
 static int big_file_mpi_broadcast_anyerror(int rt, MPI_Comm comm);
@@ -22,7 +21,6 @@ static int big_file_mpi_broadcast_anyerror(int rt, MPI_Comm comm);
 void
 big_file_mpi_set_verbose(int verbose)
 {
-    _big_file_mpi_verbose = verbose;
 }
 
 void
@@ -389,7 +387,7 @@ _aggregated(
             ptrdiff_t offset, /* offset of the entire comm */
             size_t localsize,
             BigArray * array,
-            int (*action)(BigBlock * bb, BigBlockPtr * ptr, BigArray * array),
+            int write,
             int root,
             MPI_Comm comm);
 
@@ -397,8 +395,7 @@ static int
 _throttle_action(MPI_Comm comm, int concurrency, BigBlock * block,
     BigBlockPtr * ptr,
     BigArray * array,
-    int (*action)(BigBlock * bb, BigBlockPtr * ptr, BigArray * array)
-)
+    int write)
 {
     int ThisTask, NTask;
 
@@ -449,7 +446,7 @@ _throttle_action(MPI_Comm comm, int concurrency, BigBlock * block,
         size_t offset = myoffset;
         MPI_Bcast(&offset, 1, MPI_LONG, 0, seggrp->Segment);
 
-        rt = _aggregated(block, ptr, offset, localsize, array, action, seggrp->segment_leader_rank, seggrp->Segment);
+        rt = _aggregated(block, ptr, offset, localsize, array, write, seggrp->segment_leader_rank, seggrp->Segment);
 
     }
 
@@ -469,7 +466,7 @@ _aggregated(
             ptrdiff_t offset, /* offset of the entire comm */
             size_t localsize, /* offset of the entire comm */
             BigArray * array,
-            int (*action)(BigBlock * bb, BigBlockPtr * ptr, BigArray * array),
+            int write,
             int root,
             MPI_Comm comm)
 {
@@ -522,16 +519,20 @@ _aggregated(
         big_array_init(garray, gbuf, block->dtype, 2, (size_t[]){(size_t) grouptotalsize, (size_t) block->nmemb}, NULL);
     }
 
-    if(action == big_block_write) {
+    if(write) {
         _dtype_convert(ilarray, iarray, localsize * block->nmemb);
         MPI_Gatherv(lbuf, recvcounts[rank], mpidtype,
                     gbuf, recvcounts, recvdispls, mpidtype, root, comm);
     }
     if(rank == root) {
         big_block_seek_rel(block, ptr1, offset);
-        e = action(block, ptr1, garray);
+        if(write)
+            e = big_block_write(block, ptr1, garray);
+        else
+            e = big_block_read(block, ptr1, garray);
     }
-    if(action == big_block_read) {
+    /* We are a read*/
+    if(!write) {
         MPI_Scatterv(gbuf, recvcounts, recvdispls, mpidtype,
                     lbuf, localsize, mpidtype, root, comm);
         _dtype_convert(iarray, ilarray, localsize * block->nmemb);
@@ -550,14 +551,14 @@ _aggregated(
 int
 big_block_mpi_write(BigBlock * block, BigBlockPtr * ptr, BigArray * array, int concurrency, MPI_Comm comm)
 {
-    int rt = _throttle_action(comm, concurrency, block, ptr, array, big_block_write);
+    int rt = _throttle_action(comm, concurrency, block, ptr, array, 1);
     return rt;
 }
 
 int
 big_block_mpi_read(BigBlock * block, BigBlockPtr * ptr, BigArray * array, int concurrency, MPI_Comm comm)
 {
-    int rt = _throttle_action(comm, concurrency, block, ptr, array, big_block_read);
+    int rt = _throttle_action(comm, concurrency, block, ptr, array, 0);
     return rt;
 }
 
