729b59e6ae4176cafe55923663dac867af3f5e8e
Simeon Bird <sbird@ucr.edu>
2026-04-19 13:39:04 -0700

    mpsort: Since enddianness is constexpr, we can push it all the way down

--- libgadget/utils/mpsort.c
+++ libgadget/utils/mpsort.c
@@ -43,16 +43,17 @@ static void _bisect_radix (intt * u, const intt * u1, const intt * u2, size_t ju
     *u = *u1 + ((*u2 - *u1) >> 1);
 }
 
-static int _compar_radix_u8(const void * r1, const void * r2, size_t rsize, int dir) {
-    size_t i;
+static int _compar_radix_u8(const void * r1, const void * r2, size_t rsize) {
     /* from most significant */
     const uint64_t * u1 = (const uint64_t *) r1;
     const uint64_t * u2 = (const uint64_t *) r2;
-    if(dir < 0) {
+    int dir = 1;
+    if constexpr(std::endian::native == std::endian::little) {
+        dir = -1;
         u1 = (const uint64_t *) ((const char*) u1 + rsize - 8);
         u2 = (const uint64_t *) ((const char*) u2 + rsize - 8);
     }
-    for(i = 0; i < rsize; i += 8) {
+    for(size_t i = 0; i < rsize; i += 8) {
         if(*u1 < *u2) return -1;
         if(*u1 > *u2) return 1;
         u1 += dir;
@@ -60,19 +61,18 @@ static int _compar_radix_u8(const void * r1, const void * r2, size_t rsize, int 
     }
     return 0;
 }
-static int _compar_radix_le_u8(const void * r1, const void * r2, size_t rsize) {
-    return _compar_radix_u8(r1, r2, rsize, -1);
-}
-static int _compar_radix_be_u8(const void * r1, const void * r2, size_t rsize) {
-    return _compar_radix_u8(r1, r2, rsize, +1);
-}
-static void _bisect_radix(void * r, const void * r1, const void * r2, size_t rsize, int dir) {
+
+static void _bisect_radix(void * r, const void * r1, const void * r2, size_t rsize) {
     size_t i;
     const unsigned char * u1 = (const unsigned char *) r1;
     const unsigned char * u2 = (const unsigned char *) r2;
     unsigned char * u = (unsigned char *) r;
     unsigned int carry = 0;
-    if(dir > 0) {
+    int dir;
+    if constexpr(std::endian::native == std::endian::little) {
+        dir = -1;
+    } else {
+        dir = 1;
         u1 += rsize - 1;
         u2 += rsize - 1;
     }
@@ -93,12 +93,6 @@ static void _bisect_radix(void * r, const void * r1, const void * r2, size_t rsi
         *u = (tmp >> 1) ;
         u += dir;
     }
-}
-static void _bisect_radix_le(void * r, const void * r1, const void * r2, size_t rsize) {
-    _bisect_radix(r, r1, r2, rsize, -1);
-}
-static void _bisect_radix_be(void * r, const void * r1, const void * r2, size_t rsize) {
-    _bisect_radix(r, r1, r2, rsize, +1);
 }
 
 void _setup_radix_sort(
@@ -130,13 +124,8 @@ void _setup_radix_sort(
             d->bisect = (_bisect_fn_t) _bisect_radix<uint64_t>;
             break;
         default:
-            if constexpr(std::endian::native == std::endian::little) {
-                d->compar = _compar_radix_le_u8;
-                d->bisect = _bisect_radix_le;
-            } else {
-                d->compar = _compar_radix_be_u8;
-                d->bisect = _bisect_radix_be;
-            }
+            d->compar = _compar_radix_u8;
+            d->bisect = _bisect_radix;
     }
 }
 
@@ -208,17 +197,10 @@ static void radix_sort_bytes(void * base, size_t nmemb, size_t size,
 
     std::vector<size_t> indices(nmemb);
     std::iota(indices.begin(), indices.end(), 0);
-
     /* Use the same compar selection as _setup_radix_sort. */
-    if constexpr(std::endian::native == std::endian::little) {
-        std::sort(indices.begin(), indices.end(), [keys, rsize](size_t a, size_t b) {
-            return _compar_radix_le_u8(keys + a * rsize, keys + b * rsize, rsize) < 0;
-        });
-    } else {
-        std::sort(indices.begin(), indices.end(), [keys, rsize](size_t a, size_t b) {
-            return _compar_radix_be_u8(keys + a * rsize, keys + b * rsize, rsize) < 0;
-        });
-    }
+    std::sort(indices.begin(), indices.end(), [keys, rsize](size_t a, size_t b) {
+        return _compar_radix_u8(keys + a * rsize, keys + b * rsize, rsize) < 0;
+    });
     myfree(keys);
     /* Rearrange the data into a temporary buffer according to indices,
      * then memcpy back to base. */
