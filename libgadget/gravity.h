60999f494ee16f109ecf479ef776f8944525bb60
Simeon Bird <sbird@ucr.edu>
2026-07-06 09:53:24 -0700

    Fix typo in potential table interpolation
    This has been wrong since 2018, commit:
    9dab4f341cdda043b2b36887b0916e1d1a64eba9 !
    
    Thanks to Claude fable for noticing.
    
    Fortunately we don't actually use the potential on these short scales
    for anything, but it is nice to have it corrected.

--- libgadget/gravity.h
+++ libgadget/gravity.h
@@ -55,7 +55,7 @@ class GravShortTable
             return 1;
         /* use a linear interpolation; */
         *fac *= (tabindex + 1 - i) * shortrange_table[tabindex] + (i - tabindex) * shortrange_table[tabindex + 1];
-        *pot *= (tabindex + 1 - i) * shortrange_table_potential[tabindex] + (i - tabindex) * shortrange_table_potential[tabindex];
+        *pot *= (tabindex + 1 - i) * shortrange_table_potential[tabindex] + (i - tabindex) * shortrange_table_potential[tabindex+1];
         return 0;
     }
 };
