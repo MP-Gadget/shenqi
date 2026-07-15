27c6aeaa1e3330b4a53cf5b2950f7bdd9309d8cf
Simeon Bird <sbird@ucr.edu>
2026-07-15 11:26:52 -0400

    Fix format warning

--- libgadget/cooling_qso_lightup.cpp
+++ libgadget/cooling_qso_lightup.cpp
@@ -540,7 +540,7 @@ turn_on_quasars(double atime, FOFGroups * fof, ForceTree * gasTree, Cosmology * 
         /* Get a new quasar*/
         int new_qso = choose_QSO_halo(qso_cand.size(), &ncand_before, &ncand_tot, fof->TotNgroups+iteration, rnd);
         if(new_qso >= qso_cand.size())
-            endrun(12, "HeII: QSO %d > no. candidates %d! Cannot happen\n", new_qso, qso_cand.size());
+            endrun(12, "HeII: QSO %d > no. candidates %lu! Cannot happen\n", new_qso, qso_cand.size());
         /* Make sure someone has a quasar*/
         if(ncand_tot <= 0) {
             if(desired_ion_frac - curionfrac > 0.1)
